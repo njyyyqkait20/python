480a68e26f6cc291a3d17f0722da7a9e29bece7f
njyyyqkait20 <bkvas1234@gmail.com>
2025-11-18 12:28:47 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,17 @@
+first_num = 9
+second_num = 7.8
+my_str = "start"
+print(first_num, second_num, my_str)
+first_num = 5.2
+print(f"{first_num}, float")
+third_num = first_num + second_num
+print(f"{third_num}, float")
+first_num = first_num + 5
+second_num = second_num + first_num
+print(first_num, second_num)
+second_num = 18
+print(first_num, second_num)
+my_str = "start&stop"
+print(my_str)
+my_str = my_str * 5
+print(my_str)
