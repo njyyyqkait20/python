c66742f28dd193a50e76e36b7d8256ab9784bd0c
njyyyqkait20 <bkvas1234@gmail.com>
2025-11-18 12:51:37 +0300

    Update task3.py
    я не успел дорешать олываолдываоллжтлывппваарпр

--- task3.py
+++ task3.py
@@ -1,1 +1,15 @@
-
+num_1 = 7
+num_2 = 10
+num_3 = 4
+summ = num_1 + num_2 + num_3    
+print(f"Сумма всех целых чисел: {summ}")
+num_1 = 7.9
+num_2 = 21.3
+float(num_3)
+summ = num_1 + num_2 + num_3
+print(f"Сумма всех нецелынъ чисел: {summ}")
+num_1 = 130
+num_2 = 4
+num_3 = 2
+multiplying = num_1 * num_2 * num_3
+print(f"Умножение всех чисел: {multiplying}")
